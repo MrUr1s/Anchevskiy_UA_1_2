a15d0f9bb0499ad9133241842bbd478e8cdffcd4
MrUr1s <mr.ur1s@mail.ru>
2022-08-22 15:37:33 +0700

    Commit

--- Assets/Scripts/ScenePlay.cs
+++ Assets/Scripts/ScenePlay.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class ScenePlay : MonoBehaviour
 {
-    public GameObject cube;
-    public GameObject sphere;
-    public GameObject cylinder;
+    public Transform cube;
+    public Transform sphere;
+    public Transform cylinder;
     public bool isCube,isSphere,isCylinder; 
     float angle = 0f;
 
     void Update()
     {        
         if (isCube)
-            cube.GetComponent<Transform>().transform.position += new Vector3(0, 0, 2) * Time.deltaTime;
+            cube.position += new Vector3(0, 0, 2) * Time.deltaTime;
         if (isSphere)
-            sphere.GetComponent<Transform>().transform.localScale += new Vector3(1.5f, 1.5f, 1.5f) * Time.deltaTime;
+            sphere.localScale += new Vector3(1.5f, 1.5f, 1.5f) * Time.deltaTime;
         if (isCylinder)
         {            
             angle += -5f * Time.deltaTime;
             if (angle % 360 == 0)
                 angle = 0;
-            cylinder.GetComponent<Transform>().transform.rotation = Quaternion.Euler(angle, 0f, 0f);
+            cylinder.rotation = Quaternion.Euler(angle, 0f, 0f);
         }
         
     }
